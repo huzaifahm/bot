579f683c29406695798a7d685568f7e757537e5b
huzaifahm <34286369+huzaifahm@users.noreply.github.com>
2018-03-29 11:09:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client(); 
+const settings = require('./settings.json');
 
 client.on('ready',() => {
 	console.log('I\'m Online\nI\'m Online');
 })
 
+var prefix = "%"
 client.on('message', message => {
-  if (message.content === 'ping') {
-      message.reply('pong');
+//  if (!message.content.startsWith(prefix)) return;
+  console.log('I see you 1');
+  if (message.author.bot) return;
+  if (message.content.startsWith(prefix + 'ping')) {
+      message.channel.sendMessage('pong');
   }
 }):
 
-client.login();
+client.on('message', message => {
+ // if (!message.content.startsWith(prefix)) return;
+  console.log('I see you 2');
+  if (message.author.bot) return;
+  if (message.content.startsWith(prefix + 'ping')) {
+      message.channel.sendMessage('pong');
+  }
+}):
+
+client.login(token);
